c5dc0acaeb5d1268e42c31118f1a375655aea3f6
McBeefyVEVO <ondrastepo@gmail.com>
2022-12-18 14:42:36 +0100

    (v2.0)

--- res/js/main.js
+++ res/js/main.js
@@ -23,7 +23,7 @@ const shop_button = document.getElementById("shop_button");
 const back_shop = document.getElementById("back_shop");
 const back_fight = document.getElementById("back_fight");
 
-const dozerSFX = document.getElementById("dozerSFX");
+const SwatSFX = document.getElementById("SwatSFX");
 const dallasSFX = document.getElementById("dallasSFX");
 
 const tf2 = document.getElementById("tf2");
@@ -54,14 +54,43 @@ const funny_buy = document.getElementById("funny_buy");
 const dallas_boss = document.getElementById("dallas_boss");
 const boss_select = document.getElementById("boss_select");
 const boss_select_back = document.getElementById("boss_select_back");
-const cloaker_fight = document.getElementById("cloaker_fight");
+
+const back_boss_fight = document.getElementById("back_boss_fight");
+
+const Taser_fight = document.getElementById("Taser_fight");
+const taser = document.getElementById("taser");
+const taserHPbox = document.getElementById("taserHP");
+const attack_taser = document.getElementById("attack_taser");
+const taserSFX = document.getElementById("taserSFX");
+
+const Cloaker_fight = document.getElementById("Cloaker_fight");
+const cloaker = document.getElementById("cloaker");
+const cloakerHPbox = document.getElementById("cloakerHP");
+const attack_cloaker = document.getElementById("attack_cloaker");
+const cloakerSFX = document.getElementById("cloakerSFX");
+
+const Dozer_fight = document.getElementById("Dozer_fight");
+const dozer = document.getElementById("dozer");
+const dozerHPbox = document.getElementById("dozerHP");
+const attack_dozer = document.getElementById("attack_dozer");
+const dozerSFX = document.getElementById("dozerSFX");
+
+const sokol_house = document.getElementById("sokol_house");
+const sokol_fight = document.getElementById("sokol_fight");
+const sokol_ally = document.getElementById("sokol_ally");
+
+
 
 //-------------------------------------------------------------------------------------
 
 let cash = 0;
 
 let playHP = 10;
-let eneHP = 20;
+let swatHP = 20;
+
+let taserHP = 500;
+let cloakerHP = 1000;
+let dozerHP = 2000;
 
 let dmgUpgrade = 1;
 let dmgUpgradePrice = 50;
@@ -74,6 +103,7 @@ let moneyUpgradePrice = 100;
 let moneyUpgrade = 1;
 
 let chainsupgrade = 0;
+let sokolupgrade = 0;
 
 let kills = 0;
 
@@ -94,14 +124,90 @@ attack.onmousedown = () => {
         count.innerHTML = "Cash: " + cash;
 
         playerHP.innerHTML = playHP;
-        enemyHP.innerHTML = eneHP;
+        enemyHP.innerHTML = swatHP;
 
-        enemyHP.innerHTML = eneHP*scaling;
+        enemyHP.innerHTML = swatHP*scaling;
         scaling += 0.50;
 
         kills++;
 
         killcount.innerHTML = "Kills: " + kills;
+
+        
+    }
+
+}
+
+attack_taser.onmousedown = () => {
+    
+
+    if (taserHPbox.innerHTML > 0 && playerHP.innerHTML > 0) {
+    taserHPbox.innerHTML -= dmgUpgrade;
+    player.style.left = "55%";
+    }
+
+    if (taserHPbox.innerHTML < 0.1) {
+        cash += 50 * moneyUpgrade;
+        count.innerHTML = "Cash: " + cash;
+
+        playerHP.innerHTML = playHP;
+        taserHPbox.innerHTML = taserHP;
+
+        kills++;
+
+        killcount.innerHTML = "Kills: " + kills;
+
+        
+    }
+
+}
+
+attack_cloaker.onmousedown = () => {
+    
+
+    if (cloakerHPbox.innerHTML > 0 && playerHP.innerHTML > 0) {
+    cloakerHPbox.innerHTML -= dmgUpgrade;
+    player.style.left = "55%";
+    }
+
+    if (cloakerHPbox.innerHTML < 0.1) {
+        cash += 100 * moneyUpgrade;
+        count.innerHTML = "Cash: " + cash;
+
+        playerHP.innerHTML = playHP;
+        cloakerHPbox.innerHTML = cloakerHP;
+
+        kills++;
+
+        killcount.innerHTML = "Kills: " + kills;
+
+
+
+        
+    }
+
+}
+
+attack_dozer.onmousedown = () => {
+    
+
+    if (dozerHPbox.innerHTML > 0 && playerHP.innerHTML > 0) {
+    dozerHPbox.innerHTML -= dmgUpgrade;
+    player.style.left = "55%";
+    }
+
+    if (dozerHPbox.innerHTML < 0.1) {
+        cash += 250 * moneyUpgrade;
+        count.innerHTML = "Cash: " + cash;
+
+        playerHP.innerHTML = playHP;
+        dozerHPbox.innerHTML = dozerHP;
+
+        kills++;
+
+        killcount.innerHTML = "Kills: " + kills;
+
+
 
         
     }
@@ -115,22 +221,56 @@ attack.onmouseup = () => {
     
 }
 
+attack_taser.onmouseup = () => {
+    player.style.left = "30%";
+    
+}
+
+attack_cloaker.onmouseup = () => {
+    player.style.left = "30%";
+    
+}
+
+attack_dozer.onmouseup = () => {
+    player.style.left = "30%";
+    
+}
+
 const dmgPlayer = () => {
     playerHP.innerHTML--;
     
-
     if (playerHP.innerHTML <= 0) {
         clearInterval(enemyAttackInterval);
 
         dallasSFX.play();
         dallasSFX.volume = 0.2;
         dallasSFX.currentTime = 0;
+
+        clearInterval(sokolInterval);
+        clearInterval(chainsInterval);
     }
 
+}
+
+const dmgPlayerDozer = () => {
+    playerHP.innerHTML -= 3;
+    
+    if (playerHP.innerHTML <= 0) {
+        clearInterval(enemyAttackInterval);
+
+        dallasSFX.play();
+        dallasSFX.volume = 0.2;
+        dallasSFX.currentTime = 0;
+
+        clearInterval(sokolInterval);
+        clearInterval(chainsInterval);
+    }
+
+}
 
 //-------------------------------------------------------------------------------------    
 
-}
+
 
 
 //-------------------------------------------------------------------------------------
@@ -189,6 +329,21 @@ chains_ally.onclick = () => {
     }
 }
 
+sokol_ally.onclick = () => {
+    if(cash >= 350){
+        cash -= 350;
+
+        count.innerHTML = "Cash: " + cash;
+        sokol_ally.style.display = "none";
+
+        sokolupgrade = 1;
+
+        
+    }
+}
+
+
+
 funny_buy.onclick = () => {
     if(cash >= 1000){
         cash -= 1000;
@@ -196,7 +351,7 @@ funny_buy.onclick = () => {
         count.innerHTML = "Cash: " + cash;
         funny_buy.style.display = "none";
 
-        funnyupgrade++;
+        funnyupgrade = 1;
 
         DMG_stat.innerHTML = "DMG: " + 15;
         dmgUpgrade += 15;
@@ -298,7 +453,9 @@ shop_button.onclick = () => {
         dmgUp.style.display = "block";
         hpUp.style.display = "block";
         moneyUp.style.display = "block";
+
         chains_ally.style.display = "block";
+        sokol_ally.style.display = "block";
 
         funny.style.display = "none";
         funny_buy.style.display = "block";
@@ -308,10 +465,16 @@ shop_button.onclick = () => {
 
         if(chainsupgrade == 1){
             chains_house.style.display = "none";
+            chains_ally.style.display = "none";
+        }
+
+        if(sokolupgrade == 1){
+            sokol_house.style.display = "none";
+            sokol_ally.style.display = "none";
         }
 
         if(funnyupgrade == 1){
-            funny_buy.display = "none";
+            funny_buy.style.display = "none";
         }
 
 
@@ -344,7 +507,9 @@ back_shop.onclick = () => {
         dmgUp.style.display = "none";
         hpUp.style.display = "none";
         moneyUp.style.display = "none";
+
         chains_ally.style.display = "none";
+        sokol_ally.style.display = "none";
 
         count.style.display = "none";
 
@@ -356,6 +521,10 @@ back_shop.onclick = () => {
 
         if(chainsupgrade == 1){
             chains_house.style.display = "block";
+        }
+
+        if(sokolupgrade == 1){
+            sokol_house.style.display = "block";
         }
 }
 
@@ -391,27 +560,30 @@ fight_button.onclick = () => {
         funny.style.display = "none";
         funny_buy.style.display = "none";
 
-        enemyAttackInterval = setInterval(dmgPlayer, 600);
+        
+
+        enemyAttackInterval = setInterval(dmgPlayer, 800);
 
         if(chainsupgrade == 1){
 
            chainsInterval = setInterval(() => {enemyHP.innerHTML -= 5}, 1000);
             
-        };
+           chains_fight.style.display = "block";
+           chains_house.style.display = "none";
+        }
+
+        if(sokolupgrade == 1){
+            sokol_house.style.display = "none";
+            sokol_fight.style.display = "block";
+
+            sokolInterval = setInterval (() => {enemyHP.innerHTML -= 5}, 600);
+        }
 
         playerHP.innerHTML = playHP;
 
-        dozerSFX.play();
-        dozerSFX.volume = 0.2;
-        dozerSFX.currentTime = 0;
-
-        if(chainsupgrade == 1){
-            chains_fight.style.display = "block";
-            chains_house.style.display = "none";
-        };
-
-        
-
+        SwatSFX.play();
+        SwatSFX.volume = 0.2;
+        SwatSFX.currentTime = 0;
 
 
 }
@@ -446,8 +618,8 @@ back_fight.onclick = () => {
 
 
     clearInterval(enemyAttackInterval);
-    enemyHP.innerHTML = eneHP;
-    enemyHP.innerHTML = eneHP*scaling;
+    enemyHP.innerHTML = swatHP;
+    enemyHP.innerHTML = swatHP*scaling;
 
 
     if(chainsupgrade == 1){
@@ -455,12 +627,28 @@ back_fight.onclick = () => {
         chains_house.style.display = "block";
         clearInterval(chainsInterval);
     }
+
+    if(sokolupgrade == 1){
+        sokol_fight.style.display = "none";
+        sokol_house.style.display = "block";
+        clearInterval(sokolInterval);
+    }
     
     if(funnyupgrade == 1){
         funny.style.display = "block"
     }
 
 }
+
+
+
+
+
+
+
+
+
+
 
 boss_select.onclick = () => {
     document.body.style.background = "url(./res/img/boss_select_room.jpeg)";
@@ -473,6 +661,7 @@ boss_select.onclick = () => {
     fight_button.style.display = "none";
     shop_button.style.display = "none";
     killcount.style.display = "none";
+
     boss_select.style.display = "none";
     boss_select_back.style.display = "block";
 
@@ -480,11 +669,27 @@ boss_select.onclick = () => {
     HP_img.style.display = "none";
     Cash_img.style.display = "none";
 
+    if(funnyupgrade == 1){
+        funny.style.display = "none";
+    }
+
+    if(chainsupgrade == 1){
+        chains_house.style.display = "none";
+    }
+
+    if(sokolupgrade == 1){
+        sokol_house.style.display = "none";
+    }
+    
+
     DMG_stat.style.display = "none";
     HP_stat.style.display = "none";
     Cash_stat.style.display = "none";
 
-    cloaker_fight.style.display = "block";
+    Taser_fight.style.display = "block";
+    Cloaker_fight.style.display = "block";
+    Dozer_fight.style.display = "block";
+
     dallas_boss.style.display = "block";
 }
 
@@ -519,12 +724,232 @@ boss_select_back.onclick = () => {
     enemyHP.style.display = "none";
 
     dallas_boss.style.display = "none";
-    cloaker_fight.style.display = "none";
+
+    Taser_fight.style.display = "none";
+    Cloaker_fight.style.display = "none";
+    Dozer_fight.style.display = "none";
+
+    if(funnyupgrade == 1){
+        funny.style.display = "block";
+    }
+
+    if(chainsupgrade == 1){
+        chains_house.style.display = "block";
+    }
+
+    if(sokolupgrade == 1){
+        sokol_house.style.display = "block";
+    }
 }
 
-cloaker_fight = () => {
+Taser_fight.onclick = () => {
+
+    if(kills >= 30){
+        document.body.style.background = "url(./res/img/boss_fight_room.png)";
+        document.body.style.backgroundRepeat = "no-repeat";
+        document.body.style.backgroundPosition = "center center";
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundAttachment = "fixed";
+
+
+        player.style.display = "block";
+        taser.style.display = "block";
+        attack_taser.style.display = "block";
+
+        playerHP.style.display = "block";
+        taserHPbox.style.display = "block";
+
+        Taser_fight.style.display = "none";
+        Cloaker_fight.style.display = "none";
+        Dozer_fight.style.display = "none";
+
+        dallas_boss.style.display = "none";
+
+        back_boss_fight.style.display = "block";
+        boss_select_back.style.display = "none";
+
+        enemyAttackInterval = setInterval(dmgPlayer, 600);
+
+        if(chainsupgrade == 1){
+
+            chainsInterval = setInterval(() => {taserHPbox.innerHTML -= 5}, 1000);
+             
+            chains_fight.style.display = "block";
+         }
+ 
+         if(sokolupgrade == 1){
+             sokol_fight.style.display = "block";
+ 
+             sokolInterval = setInterval (() => {taserHPbox.innerHTML -= 5}, 600);
+         }
+
+        playerHP.innerHTML = playHP;
+
+        taserSFX.play();
+        taserSFX.volume = 0.2;
+        taserSFX.currentTime = 0;
+        }
+
+        
+}
+
+Cloaker_fight.onclick = () => {
+    if(kills >= 50){
+        document.body.style.background = "url(./res/img/boss_fight_room.png)";
+        document.body.style.backgroundRepeat = "no-repeat";
+        document.body.style.backgroundPosition = "center center";
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundAttachment = "fixed";
+
+
+        player.style.display = "block";
+        cloaker.style.display = "block";
+        attack_cloaker.style.display = "block";
+
+        playerHP.style.display = "block";
+        cloakerHPbox.style.display = "block";
+
+        Taser_fight.style.display = "none";
+        Cloaker_fight.style.display = "none";
+        Dozer_fight.style.display = "none";
+
+        dallas_boss.style.display = "none";
+
+        back_boss_fight.style.display = "block";
+        boss_select_back.style.display = "none";
+
+        enemyAttackInterval = setInterval(dmgPlayer, 400);
+
+        if(chainsupgrade == 1){
+
+           chainsInterval = setInterval(() => {cloakerHPbox.innerHTML -= 5}, 1000);
+            
+           chains_fight.style.display = "block";
+        }
+
+        if(sokolupgrade == 1){
+            sokol_fight.style.display = "block";
+
+            sokolInterval = setInterval (() => {cloakerHPbox.innerHTML -= 5}, 600);
+        }
+
+        playerHP.innerHTML = playHP;
+
+        cloakerSFX.play();
+        cloakerSFX.volume = 0.2;
+        cloakerSFX.currentTime = 0;
+        }
+}
+
+Dozer_fight.onclick = () => {
+    if(kills >= 100){
+        document.body.style.background = "url(./res/img/boss_fight_room.png)";
+        document.body.style.backgroundRepeat = "no-repeat";
+        document.body.style.backgroundPosition = "center center";
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundAttachment = "fixed";
+
+
+        player.style.display = "block";
+        dozer.style.display = "block";
+        attack_dozer.style.display = "block";
+
+        playerHP.style.display = "block";
+        dozerHPbox.style.display = "block";
+
+        Taser_fight.style.display = "none";
+        Cloaker_fight.style.display = "none";
+        Dozer_fight.style.display = "none";
+
+        dallas_boss.style.display = "none";
+
+        back_boss_fight.style.display = "block";
+        boss_select_back.style.display = "none";
+
+        enemyAttackInterval = setInterval(dmgPlayerDozer, 1000);
+
+        if(chainsupgrade == 1){
+
+           chainsInterval = setInterval(() => {dozerHPbox.innerHTML -= 5}, 1000);
+            
+           chains_fight.style.display = "block";
+        }
+
+        if(sokolupgrade == 1){
+            sokol_fight.style.display = "block";
+
+            sokolInterval = setInterval (() => {dozerHPbox.innerHTML -= 5}, 600);
+        }
+
+        playerHP.innerHTML = playHP;
+
+        dozerSFX.play();
+        dozerSFX.volume = 0.2;
+        dozerSFX.currentTime = 0;
+        }
+}
+
+
+
+
+
+back_boss_fight.onclick = () => {
+    document.body.style.background = "url(./res/img/boss_select_room.jpeg)";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundPosition = "center center";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+
+    taser.style.display = "none";
+    cloaker.style.display = "none";
+    dozer.style.display = "none";
+    player.style.display = "none";
+
+    attack_taser.style.display = "none";
+    attack_cloaker.style.display = "none";
+    attack_dozer.style.display = "none";
+
+    playerHP.style.display = "none";
+    taserHPbox.style.display = "none";
+    cloakerHPbox.style.display = "none";
+    dozerHPbox.style.display = "none";
+
+    back_boss_fight.style.display = "none";
+    boss_select_back.style.display = "block";
+
+
+
+    if(chainsupgrade == 1){
+        chains_fight.style.display = "none";
+        clearInterval(chainsInterval);
+    }
+
+    if(sokolupgrade == 1){
+        sokol_fight.style.display = "none";
+        clearInterval(sokolInterval);
+    }
+    
+
+    DMG_stat.style.display = "none";
+    HP_stat.style.display = "none";
+    Cash_stat.style.display = "none";
+
+    Taser_fight.style.display = "block";
+    Cloaker_fight.style.display = "block";
+    Dozer_fight.style.display = "block";
+
+    dallas_boss.style.display = "block";
+
+
+    clearInterval(enemyAttackInterval);
+    taserHPbox.innerHTML = taserHP;
+    cloakerHPbox.innerHTML = cloakerHP;
+    dozerHPbox.innerHTML = dozerHP;
+    
+
 
 }
+
 
 
 
